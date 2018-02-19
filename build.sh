4594a912a26eb67c1f2eb5abd8b08e8ed9705ea4
smitchell7 <samuel.alan.mitchell@gmail.com>
2018-02-19 16:16:33 -0700

    Updated build.sh to grab external files
    This will get android ndk, opencv, and android-cmake.

--- build.sh
+++ build.sh
@@ -3,10 +3,34 @@ echo "Configuring and building Thirdparty/DBoW2 ..."
 
 export ABI="armeabi-v7a"
 export API='android-15'
+export ANDROID_NDK_VERSION='android-ndk-r12b'
+export PROJ_DIR=${PWD}  
+export BUILD_OPENCV=0   
 
 
+# Get extra files
+cd Thirdparty
+wget http://bitbucket.org/eigen/eigen/get/3.2.10.tar.gz
+mkdir eigen && tar xzf 3.2.10.tar.gz -C eigen --strip-components 1
 
-export CMFLAGS='-DCMAKE_TOOLCHAIN_FILE='${PWD}'/../android-cmake/android.toolchain.cmake -DANDROID_NDK='${PWD}'/../android-ndk-r12b -DCMAKE_BUILD_TYPE=Release -DANDROID_ABI='$ABI' -DANDROID_NATIVE_API_LEVEL='$API' -Wno-dev -DOpenCV_DIR='${PWD}'/Thirdparty/opencv/build/'
+if [$BUILD_OPENCV==1]; then
+  # if we build opencv, then we should point to the correct installation.
+  # if not, then let cmake try to figure it out for itself.
+  # note that we are using the opencv setup that's specific for the ABI and API
+  export OPENCV_DIR_FLAG=' -DOpenCV_DIR='{PWD}'/Thirdparty/opencv/build/'
+  git clone https://github.com/opencv/opencv.git
+fi
+
+
+cd ../..
+git clone https://github.com/taka-no-me/android-cmake
+wget https://dl.google.com/android/repository/$ANDROID_NDK_VERSION-linux-x86_64.zip
+unzip $ANDROID_NDK_VERSION-linux-x86_64.zip
+cd $PROJ_DIR
+
+# define all of the cmake config files. This plays heavily with the android-cmake setup that's downloaded above.
+export CMFLAGS='-DCMAKE_TOOLCHAIN_FILE='${PWD}'/../android-cmake/android.toolchain.cmake -DANDROID_NDK='${PWD}'/../'$ANDROID_NDK_VERSION' -DCMAKE_BUILD_TYPE=Release -DANDROID_ABI='$ABI' -DANDROID_NATIVE_API_LEVEL='$API' -Wno-dev '$OPENCV_DIR_FLAG
+
 echo $CMFLAGS
 
 #export FAST='-j'
